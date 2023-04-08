4dd16985c1364f00cbee32b7a07ae04a306a5349
Jens Petersen <petersen@redhat.com>
2023-04-08 13:16:45 +0800

    build.sh: x86_64 first and then other archs

--- build.sh
+++ build.sh
@@ -16,14 +16,16 @@ else
 fi
 
 for br in $branches; do
-    for ghc in "${versions[@]}"; do
-        echo
-        if [[ -z "$ghc" ]] && grep -q '^%global ghc_name' haskell-language-server.spec; then
-            sed -i -e 's/%global ghc_name .*/#%%global ghc_name ghc9.4/' haskell-language-server.spec
-        else
-            sed -i -e 's/#%%global ghc_name/%global ghc_name/' -e s/'\(%global ghc_name ghc\).*'/'\1'"$ghc"/ haskell-language-server.spec
-        fi
-        grep 'global ghc_name' haskell-language-server.spec | grep "$ghc"
-        fbrnch copr haskell-language-server $br
+    for arch in "-a x86_64" "-X x86_64 -R"; do
+        for ghc in "${versions[@]}"; do
+            echo
+            if [[ -z "$ghc" ]] && grep -q '^%global ghc_name' haskell-language-server.spec; then
+                sed -i -e 's/%global ghc_name .*/#%%global ghc_name ghc9.4/' haskell-language-server.spec
+            else
+                sed -i -e 's/#%%global ghc_name/%global ghc_name/' -e s/'\(%global ghc_name ghc\).*'/'\1'"$ghc"/ haskell-language-server.spec
+            fi
+            grep 'global ghc_name' haskell-language-server.spec | grep "$ghc"
+            fbrnch copr haskell-language-server $br $arch
+        done
     done
 done
