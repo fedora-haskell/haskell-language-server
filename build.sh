735f58794908a8fa59e3de2ba68ac06e5742fd9b
Jens Petersen <petersen@redhat.com>
2023-05-26 02:12:56 +0800

    build.sh: ARCH and exclude ghc8.10 for f37/epel9

--- build.sh
+++ build.sh
@@ -16,12 +16,15 @@ else
     versions=("$@")
 fi
 
+if [[ -z "$ARCH" ]]; then
+    ARCH=("-X ppc64le -R")
+fi
+
 for br in $branches; do
 #    for arch in "-a x86_64" "-X x86_64 -R"; do
-    for arch in "-X ppc64le -R"; do
 #    for arch in "-R"; do
         for ghc in "${versions[@]}"; do
-          if [ "$br" != "f37" -o "$ghc" != "8.10" ]; then
+          if [ "$br" != "f37" -a "$br" != "epel9" -o "$ghc" != "8.10" ]; then
             echo
             if [[ -z "$ghc" ]] && grep -q '^%global ghc_name' haskell-language-server.spec; then
                 sed -i -e 's/%global ghc_name .*/#%%global ghc_name ghc9.4/' haskell-language-server.spec
@@ -29,8 +32,7 @@ for br in $branches; do
                 sed -i -e 's/#%%global ghc_name/%global ghc_name/' -e s/'\(%global ghc_name ghc\).*'/'\1'"$ghc"/ haskell-language-server.spec
             fi
             grep 'global ghc_name' haskell-language-server.spec | grep "$ghc"
-            fbrnch copr haskell-language-server $br $arch
+            fbrnch copr haskell-language-server $br $ARCH
           fi
         done
-    done
 done
