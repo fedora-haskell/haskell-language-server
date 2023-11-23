fb30c07c1d94493ebd5f03cbc23b24a4b17bfa78
Jens Petersen <petersen@redhat.com>
2023-11-24 00:12:55 +0800

    add ghc minor version suffix to release

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -e +x
 
 if [[ "$1" = "-h" ]]; then
     echo "Usage: ./build.sh [BRANCH] [VERSION]..."
@@ -30,6 +30,25 @@ for br in $branches; do
                 sed -i -e 's/%global ghc_name .*/#%%global ghc_name ghc9.4/' haskell-language-server.spec
             else
                 sed -i -e 's/#%%global ghc_name/%global ghc_name/' -e s/'\(%global ghc_name ghc\).*'/'\1'"$ghc"/ haskell-language-server.spec
+            fi
+            case $ghc in
+                9.8) LATEST=9.8.1 ;;
+                9.6) LATEST=9.6.3 ;;
+                9.4) LATEST=9.4.8 ;;
+                9.2) LATEST=9.2.8 ;;
+                9.0) LATEST=9.0.2 ;;
+                8.10) LATEST=8.10.7 ;;
+                *) echo "unknown major version: $ghc"
+                   exit 1
+                   ;;
+            esac
+            ghcversion=$(fdrq $br --qf "%{version}" --latest-limit 1 ghc$ghc)
+            if [ "$ghcversion" != "$LATEST" ]; then
+                echo "repo ghc$ghc-$ghcversion /= $LATEST"
+                exit 1
+            else
+                echo "$ghcversion"
+                sed -i -e "s/%global ghc_minor .*/%global ghc_minor $ghcversion/" haskell-language-server.spec
             fi
             ghc_name=$(grep '%global ghc_name' haskell-language-server.spec)
             echo "$ghc_name"
