b2753e1e6683daa0457590c1f74fc2073a05e9e4
Jens Petersen <petersen@redhat.com>
2025-03-29 21:28:05 +0800

    build.hs: fedora-releases 0.2 uses showBranch

--- build.hs
+++ build.hs
@@ -7,7 +7,8 @@ import Data.List ((\\))
 import Data.Maybe (mapMaybe)
 import Data.Tuple (swap)
 import Data.Version.Extra
-import Distribution.Fedora.Branch (Branch(..), eitherBranch, getFedoraBranches)
+import Distribution.Fedora.Branch (Branch(..), eitherBranch, getActiveBranches,
+                                   showBranch)
 import SimpleCmd
 import SimpleCmdArgs
 
@@ -35,17 +36,20 @@ runRemote dryrun reqarchs (reqbrs,reqghcs) = do
   branches <-
     if null reqbrs
     then do
-      allbrs <- getFedoraBranches
+      allbrs <- getActiveBranches
       return $ allbrs \\ [EPEL 8, EPELNext 8, EPELNext 9]
     else return reqbrs
   let ghcs = if null reqghcs then defaultGHCs else reqghcs
   forM_ branches $ \br ->
-    forM_ ghcs $ \ghc -> do
+    forM_ ghcs $ \ghc ->
+    if ghc `elem` [GHC] && br == EPEL 9
+    then putStrLn $ "skipping" +-+ showBranch br +-+ showGHCPkg ghc
+    else do
     putChar '\n'
-    putStrLn $ "#" +-+ show br +-+ showGHCPkg ghc
-    version <- cmd "frpq" ["-q", (show br), "--qf=%{version}", "--latest-limit=1", showGHCPkg ghc]
+    putStrLn $ "#" +-+ showBranch br +-+ showGHCPkg ghc
+    version <- cmd "frpq" ["-q", (showBranch br), "--qf=%{version}", "--latest-limit=1", showGHCPkg ghc]
     if null version
-      then error' $ showGHCPkg ghc +-+ "not found" +-+ "for" +-+ show br
+      then error' $ showGHCPkg ghc +-+ "not found" +-+ "for" +-+ showBranch br
       else do
       putStrLn version
       let ghcversion = readVersion version
@@ -64,7 +68,7 @@ runRemote dryrun reqarchs (reqbrs,reqghcs) = do
                  if null reqarchs then defaultArchs else reqarchs
       -- 9.4 fails to link for aarch64 with ld.gold: skip unless explicit req
       unless (reqarchs == [AARCH64] && ghc == GHC9_4 && ghcs /= [GHC9_4]) $
-        cmdLog_ "fbrnch" $ "copr" : ["-n" | dryrun] ++ ["--single", "haskell-language-server", show br] ++ map archOpt archs
+        cmdLog_ "fbrnch" $ "copr" : ["-n" | dryrun] ++ ["--single", "haskell-language-server", showBranch br] ++ map archOpt archs
         where
           maybeGHCArchs :: GHCPKG -> Arch -> Maybe Arch
           maybeGHCArchs GHC9_4 AARCH64 = Nothing
@@ -113,18 +117,18 @@ partitionBranches :: [String] -> ([Branch],[GHCPKG])
 partitionBranches args =
   fmap (map readGHCPkg) . swap . partitionEithers $ map eitherBranch args
 
-ghcVersion :: Branch -> Version
-ghcVersion (EPEL 9) = makeVersion [8,10,7]
-ghcVersion (Fedora 38) = makeVersion [9,2,6]
-ghcVersion (Fedora 39) = makeVersion [9,4,5]
-ghcVersion (Fedora 40) = makeVersion [9,4,5]
-ghcVersion (Fedora 41) = makeVersion [9,6,6]
-ghcVersion Rawhide = makeVersion [9,6,6]
+-- ghcVersion :: Branch -> Version
+-- ghcVersion (EPEL 9) = makeVersion [8,10,7]
+-- ghcVersion (Fedora 38) = makeVersion [9,2,6]
+-- ghcVersion (Fedora 39) = makeVersion [9,4,5]
+-- ghcVersion (Fedora 40) = makeVersion [9,4,5]
+-- ghcVersion (Fedora 41) = makeVersion [9,6,6]
+-- ghcVersion Rawhide = makeVersion [9,6,6]
 
 latestGHC :: GHCPKG -> Version
 latestGHC GHC9_10 = makeVersion [9,10,1]
-latestGHC GHC9_8 = makeVersion [9,8,2]
-latestGHC GHC9_6 = makeVersion [9,6,6]
+latestGHC GHC9_8 = makeVersion [9,8,4]
+latestGHC GHC9_6 = makeVersion [9,6,7]
 latestGHC GHC9_4 = makeVersion [9,4,8]
 latestGHC GHC9_2 = makeVersion [9,2,8]
 latestGHC GHC9_0 = makeVersion [9,0,2]
